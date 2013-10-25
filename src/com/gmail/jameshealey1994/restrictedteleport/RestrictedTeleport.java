2be44f4936233a342e80fd7fc8862e738ce628b3
James Healey <jameshealey1994@gmail.com>
2013-10-26 00:35:44 +0100

    Updated Javadoc, added package-info, and fixed formatting.

--- src/com/gmail/jameshealey1994/restrictedteleport/RestrictedTeleport.java
+++ src/com/gmail/jameshealey1994/restrictedteleport/RestrictedTeleport.java
@@ -7,61 +7,51 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
- * Restricted Teleport Bukkit Plugin
- * @author James Healey
+ * Restricted Teleport Bukkit Plugin.
+ *
+ * @author JamesHealey94 <jameshealey1994.gmail.com>
  */
-public final class RestrictedTeleport extends JavaPlugin
-{
+public final class RestrictedTeleport extends JavaPlugin {
+
     /**
-     * Carries out commands
-     * @param sender The sender of the command
-     * @param cmd The command the user sent
-     * @param commandLabel The exact command the user sent
-     * @param args The arguments given with the command
-     * @return False if usage message needs to be shown, else True.
+     * Executes commands.
+     *
+     * @param sender        sender of the command
+     * @param cmd           command the user sent
+     * @param commandLabel  exact command the user sent
+     * @param args          arguments given with the command
+     * @return              false if usage message needs to be shown, else true
      */
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
-    {
-        if (sender instanceof Player)
-        {
-            Player player = (Player) sender;
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        if (sender instanceof Player) {
+            final Player player = (Player) sender;
 
-            if (cmd.getName().equalsIgnoreCase("rtp"))
-            {
-                if (args.length > 0)
-                {
-                    Player target = getServer().getPlayer(args[0]);
-                    if (target == null)
-                    {
+            if (cmd.getName().equalsIgnoreCase("rtp")) {
+                if (args.length > 0) {
+                    final Player target = getServer().getPlayer(args[0]);
+                    if (target == null) {
                         player.sendMessage(ChatColor.RED + "Player '" + args[0] + "' is not online or is invalid");
                         return false;
-                    }
-                    else
-                    {
-                        if (! (player.hasPermission("rtp.teleport") || player.hasPermission("rtp.teleport.override")))
-                        {
+                    } else {
+                        if (!(player.hasPermission("rtp.teleport") || player.hasPermission("rtp.teleport.override"))) {
                             player.sendMessage(ChatColor.RED + "You do not have permissions to teleport right now");
                             return true;
                         }
-                        if (! (target.hasPermission("rtp.beteleportedto") || player.hasPermission("rtp.teleport.override")))
-                        {
+                        if (!(target.hasPermission("rtp.beteleportedto") || player.hasPermission("rtp.teleport.override"))) {
                             player.sendMessage(ChatColor.RED + target.getDisplayName() + " cannot be teleported to right now");
                             return true;
                         }
 
                         player.teleport(target.getLocation());
                         player.sendMessage(ChatColor.GOLD + "Teleported to " + target.getDisplayName());
-                        
-                        if (! player.hasPermission("rtp.teleport.silent"))
-                        {
+
+                        if (!player.hasPermission("rtp.teleport.silent")) {
                             target.sendMessage(ChatColor.GRAY + player.getDisplayName() + " teleported to you");
                         }
                         return true;
                     }
-                }
-                else
-                {
+                } else {
                     sender.sendMessage(ChatColor.RED + "Please specify a player to teleport to");
                     return false;
                 }
---
+++ src/com/gmail/jameshealey1994/restrictedteleport/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * The "master"-package, containing everything related to RestrictedTeleport.
+ */
+package com.gmail.jameshealey1994.restrictedteleport;
