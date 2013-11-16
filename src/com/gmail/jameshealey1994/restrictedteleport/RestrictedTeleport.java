a2cc140e2876a77a3215dc22aeb637ec8099d43b
James Healey <jameshealey1994@gmail.com>
2013-11-16 04:01:03 +0000

    Major rewrite. Added support for /tp teleportingPlayer targetPlayer [-s].

--- src/com/gmail/jameshealey1994/restrictedteleport/RestrictedTeleport.java
+++ src/com/gmail/jameshealey1994/restrictedteleport/RestrictedTeleport.java
@@ -21,11 +21,12 @@ public final class RestrictedTeleport extends JavaPlugin {
     /**
      * Executes commands.
      *
-     * @param sender        sender of the command
-     * @param cmd           command the user sent
-     * @param commandLabel  exact command the user sent
-     * @param args          arguments given with the command
-     * @return              false if usage message needs to be shown, else true
+     * @param sender            sender of the command
+     * @param cmd               command the user sent
+     * @param commandLabel      exact command the user sent
+     * @param args              arguments given with the command
+     * @return                  false if usage message needs to be shown, else
+     *                          true
      */
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
@@ -33,46 +34,104 @@ public final class RestrictedTeleport extends JavaPlugin {
             return false;
         }
 
-        if (!(sender instanceof Player)) {
-            sender.sendMessage(ChatColor.RED + "This command can only be used by online players");
+        final String teleporterName;
+        final String targetName;
+        final boolean silent;
+
+        switch (args.length) {
+            case 1: {
+                // /tp as1lv3rn1nja
+                if (!(sender instanceof Player)) {
+                    return false;
+                }
+
+                teleporterName = ((Player) sender).getName();
+                targetName = args[0];
+                silent = false;
+                break;
+            }
+            case 2: {
+                if (args[1].equals(SILENT_FLAG)) {
+                    // /tp as1lv3rn1nja -s                      (Player) sender     as1lv3rn1nja        silent
+                    if (!(sender instanceof Player)) {
+                        return false;
+                    }
+                    teleporterName = ((Player) sender).getName();
+                    targetName = args[0];
+                    silent = true;
+                } else {
+                    // /tp as1lv3rn1nja as1lv3rn1nja            as1lv3rn1nja        as1lv3rn1nja
+                    // /tp as1lv3rn1nja skillchickenz75         as1lv3rn1nja        skillchickenz75
+                    if ((sender instanceof Player) && (!(((Player) sender).hasPermission("rtp.teleport.others")))) {
+                        sender.sendMessage(ChatColor.RED + "You do not have permissions to teleport other players");
+                        sender.sendMessage(ChatColor.GRAY + "Perhaps you meant /" + commandLabel + " username");
+                        return false;
+                    }
+                    teleporterName = args[0];
+                    targetName = args[1];
+                    silent = false;
+                }
+                break;
+            }
+            case 3: {
+                // /tp as1lv3rn1nja as1lv3rn1nja -s             as1lv3rn1nja        as1lv3rn1nja        silent
+                // /tp as1lv3rn1nja skillchickenz75 -s          as1lv3rn1nja        skillchickenz75     silent
+                if ((sender instanceof Player) && (!(((Player) sender).hasPermission("rtp.teleport.others")))) {
+                    sender.sendMessage(ChatColor.RED + "You do not have permissions to teleport other players");
+                    sender.sendMessage(ChatColor.GRAY + "Perhaps you meant /" + commandLabel + " username");
+                    return false;
+                }
+                teleporterName = args[0];
+                targetName = args[1];
+                silent = (args[2].equals(SILENT_FLAG));
+                break;
+            }
+            default: {
+                return false;
+            }
+        }
+
+        final Player teleporter = getServer().getPlayer(teleporterName);
+        if (teleporter == null) {
+            sender.sendMessage(ChatColor.RED + "Player '" + teleporterName + "' is not online or is invalid");
+            return false;
+        }
+
+        final Player target = getServer().getPlayer(targetName);
+        if (target == null) {
+            sender.sendMessage(ChatColor.RED + "Player '" + targetName + "' is not online or is invalid");
+            return false;
+        }
+
+        if (!(teleporter.hasPermission("rtp.teleport") || teleporter.hasPermission("rtp.teleport.override"))) {
+            sender.sendMessage(ChatColor.RED + "You do not have permissions to teleport right now");
+            return true;
+        }
+        if (!(target.hasPermission("rtp.beteleportedto") || teleporter.hasPermission("rtp.teleport.override"))) {
+            sender.sendMessage(ChatColor.RED + target.getDisplayName() + " cannot be teleported to right now");
             return true;
         }
 
-        if (args.length > 0) {
-            final Player target = getServer().getPlayer(args[0]);
-            if (target == null) {
-                sender.sendMessage(ChatColor.RED + "Player '" + args[0] + "' is not online or is invalid");
+        if (silent) {
+            if ((!(sender instanceof Player)) || ((sender instanceof Player) && (((Player) sender).hasPermission("rtp.teleport.silent")))) {
+                teleporter.teleport(target.getLocation());
+                teleporter.sendMessage(ChatColor.GOLD + "Teleported to " + target.getDisplayName() + " (Silently)");
+                if (!(teleporter.equals(target) || teleporter.equals(sender))) {
+                    sender.sendMessage(ChatColor.GRAY + "Teleported " + teleporter.getDisplayName() + " to " + target.getDisplayName() + " (Silently)");
+                }
+                return true;
+            } else {
+                sender.sendMessage(ChatColor.RED + "You do not have permissions to teleport silently");
                 return false;
             }
-
-            final Player player = (Player) sender;
-            if (!(player.hasPermission("rtp.teleport") || player.hasPermission("rtp.teleport.override"))) {
-                sender.sendMessage(ChatColor.RED + "You do not have permissions to teleport right now");
-                return true;
-            }
-            if (!(target.hasPermission("rtp.beteleportedto") || player.hasPermission("rtp.teleport.override"))) {
-                sender.sendMessage(ChatColor.RED + target.getDisplayName() + " cannot be teleported to right now");
-                return true;
-            }
-
-            final boolean silent = args.length > 1 && args[1].equalsIgnoreCase(SILENT_FLAG);
-            if (silent) {
-                if (player.hasPermission("rtp.teleport.silent")) {
-                    sender.sendMessage(ChatColor.GOLD + "Teleported to " + target.getDisplayName() + " (Silently)");
-                    player.teleport(target.getLocation());
-                } else {
-                    sender.sendMessage(ChatColor.RED + "You do not have permissions to teleport silently");
-                    return false;
-                }
-            } else {
-                player.teleport(target.getLocation());
-                sender.sendMessage(ChatColor.GOLD + "Teleported to " + target.getDisplayName());
-                target.sendMessage(ChatColor.GRAY + player.getDisplayName() + " teleported to you");
+        } else {
+            teleporter.teleport(target.getLocation());
+            teleporter.sendMessage(ChatColor.GOLD + "Teleported to " + target.getDisplayName());
+            target.sendMessage(ChatColor.GRAY + teleporter.getDisplayName() + " teleported to you");
+            if (!(teleporter.equals(target) || teleporter.equals(sender))) {
+                sender.sendMessage(ChatColor.GRAY + "Teleported " + teleporter.getDisplayName() + " to " + target.getDisplayName());
             }
             return true;
-        } else {
-            sender.sendMessage(ChatColor.RED + "Please specify a player to teleport to");
-            return false;
         }
     }
 }
